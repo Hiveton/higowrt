25a3aefa7be5821fa7e5e4809ac34a09fa62ef24
Hiveton <EvieAlbertapq@collector.org>
2026-06-22 10:59:06 +0000

    fix(wifi): LuCI split nil防护 + 默认纯WPA2 + 开机自动wifi up
    1. mtkwifi.lua: string:split 加 self nil 防护 (修 LuCI MTK WiFi 页
       get_all_devs 因 cfgs.WirelessMode nil 崩溃)
    2. mt7992.b0.dat: AuthMode WPA2PSKWPA3PSK->WPA2PSK (去 WPA3 SAE/PMF,
       修客户端 assoc 后 de-auth reason=1 连不上)
    3. wifi_jedi: config 子命令 -> wifi up (10-wifi-detect hotplug 开机
       调 wifi config, 之前 wifi_jedi 不认导致 WiFi 开机不自动起)

--- package/mtk/applications/luci-app-mtk/root/usr/lib/lua/mtkwifi.lua
+++ package/mtk/applications/luci-app-mtk/root/usr/lib/lua/mtkwifi.lua
@@ -75,6 +75,7 @@ end
 
 function string:split(sep)
     local sep, fields = sep or ":", {}
+    if self == nil then return fields end
     local pattern = string.format("([^%s]+)", sep)
     self:gsub(pattern, function(c) fields[#fields+1] = c end)
     return fields
